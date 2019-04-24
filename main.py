5de7ed181f5a05a9edbc071069c84b28c8c9c9a0
Maciej4 <m@maciej.me>
2019-04-24 13:16:07 -0700

    create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+A = (0, 0)
+B = (2, 0)
+C = (1, 1)
+D = (1, -1)
+
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1]) #Typo was here
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       raise Exception('lines do not intersect')
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
+
+print(line_intersection((A, B), (C, D)))
