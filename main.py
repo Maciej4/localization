3e2e40d475f71e8e60821d9d5d991c912cf8bf3d
Maciej4 <m@maciej.me>
2019-04-24 22:04:34 -0700

    improve ui

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ robot_size = 50
 rs = robot_size/math.sqrt(2)
 mp = math.pi/4
 
+def distance(a,b):
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+def is_between(a,c,b):
+    print("---")
+    print(distance(a,c) + distance(c,b))
+    print(distance(a,b))
+    return distance(a,c) + distance(c,b) == distance(a,b)
+
 def line_intersection(line1, line2):
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1]) #Typo was here
@@ -24,16 +33,24 @@ def line_intersection(line1, line2):
 
     div = det(xdiff, ydiff)
     if div == 0:
-       raise Exception('lines do not intersect')
+       return "fail"
 
     d = (det(*line1), det(*line2))
     x = det(d, xdiff) / div
     y = det(d, ydiff) / div
+    if not is_between(line2[0], (x, y), line2[1]):
+        return "fail"
+
+    cir = Circle(Point(x, y), 10)
+    cir.draw(win)
+
     return x, y
 
 def arena_intersection(line1):
     for i in arena_lines:
-        return line_intersection(line1, i)
+        point = line_intersection(line1, i)
+        if(point != "fail" and point[0] > 49 and point[0] < 551 and point[1] > 49 and point [1] < 551):
+            return point
 
 def convert_line(line1):
     return Line(Point(line1[0][0], line1[0][1]), Point(line1[1][0], line1[1][1]))
@@ -49,30 +66,51 @@ def calc_vec(origin, length, angle):
     ycomp = length*math.sin(angle)+origin[1]
     return (xcomp, ycomp)
 
-def draw_box(center, rot):
+def draw_robot(center, rot):
     p0 = calc_vec(center, rs, rot+1*mp)
     p1 = calc_vec(center, rs, rot+3*mp)
     p2 = calc_vec(center, rs, rot+5*mp)
     p3 = calc_vec(center, rs, rot+7*mp)
-    p0x = robot_size/math.sqrt(2)*math.cos(rot+1*math.pi/4)+center[0]
-    p0y = robot_size/math.sqrt(2)*math.sin(rot+1*math.pi/4)+center[1]
-    p1x = robot_size/math.sqrt(2)*math.cos(rot+3*math.pi/4)+center[0]
-    p1y = robot_size/math.sqrt(2)*math.sin(rot+3*math.pi/4)+center[1]
-    p2x = robot_size/math.sqrt(2)*math.cos(rot+5*math.pi/4)+center[0]
-    p2y = robot_size/math.sqrt(2)*math.sin(rot+5*math.pi/4)+center[1]
-    p3x = robot_size/math.sqrt(2)*math.cos(rot+7*math.pi/4)+center[0]
-    p3y = robot_size/math.sqrt(2)*math.sin(rot+7*math.pi/4)+center[1]
-
     draw_line(p0, p1)
+    draw_line(p0, p3)
+    draw_line(p1, p2)
+    draw_line(p2, p3)
 
-    return (p0, p1, p2, p3)
+    P0 = calc_vec(center, 1000, rot+0*mp)
+    P1 = calc_vec(center, 1000, rot+2*mp)
+    P2 = calc_vec(center, 1000, rot+4*mp)
+    P3 = calc_vec(center, 1000, rot+6*mp)
+    draw_line(center, P0)
+    draw_line(center, P1)
+    draw_line(center, P2)
+    draw_line(center, P3)
 
-print(draw_box((300, 300), 0))
+    i0 = arena_intersection((center, P0))
+    i1 = arena_intersection((center, P1))
+    i2 = arena_intersection((center, P2))
+    i3 = arena_intersection((center, P3))
 
-target = ((0, 0), (100, 100))
+    return (i0, i1, i2, i3)
 
-print(arena_intersection(target))
+a = ((300, 300), (200, 0))
+print(arena_intersection(a))
+draw_line(a[0], a[1])
 
-convert_line(target).draw(win)
-win.getMouse()
-win.close()
+while True:
+    pt0 = win.getMouse()
+    ptx = int(pt0.getX())
+    pty = int(pt0.getY())
+    pt1 = (ptx, pty)
+    print(arena_intersection(((300, 300), pt1)))
+    print("-----")
+    #print(line_intersection(A, ((300, 300), pt1)))
+    draw_line((300, 300), pt1)
+    if (pty > 575 and ptx > 575):
+        win.close()
+
+
+
+
+
+
+#print(draw_robot((300, 300), 1))
